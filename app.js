191a1590d8ba6265d2a63ca8e36074b868ab6810
mika1407 <koneautomaatio@hotmail.com>
2020-01-22 16:25:14 +0200

    animation

---
+++ app.js
@@ -0,0 +1,6 @@
+// Need to make a function that gets rid of the preload
+
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload");
+  preload.classList.add("preload-finish");
+});
